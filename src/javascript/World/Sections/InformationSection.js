1bb65e1e683353f937f9c4daf60d2727e02650a8
WinterChenS <1085143002@qq.com>
2020-07-26 06:49:58 +0100

    ss

--- src/javascript/World/Sections/InformationSection.js
+++ src/javascript/World/Sections/InformationSection.js
@@ -88,18 +88,18 @@ export default class InformationSection
 
         // Options
         this.links.options = [
-            // {
-            //     href: 'https://twitter.com/bruno_simon/',
-            //     labelTexture: this.resources.items.informationContactTwitterLabelTexture
-            // },
+            {
+                href: 'https://twitter.com/winterchen1111/',
+                labelTexture: this.resources.items.informationContactTwitterLabelTexture
+            },
             {
                 href: 'https://github.com/winterchens/',
                 labelTexture: this.resources.items.informationContactGithubLabelTexture
             },
-            // {
-            //     href: 'https://www.linkedin.com/in/simonbruno77/',
-            //     labelTexture: this.resources.items.informationContactLinkedinLabelTexture
-            // },
+            {
+                href: '#',
+                labelTexture: this.resources.items.informationContactLinkedinLabelTexture
+            },
             {
                 href: 'mailto:i@winterchen.com',
                 labelTexture: this.resources.items.informationContactMailLabelTexture
